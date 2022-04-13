6de8a2ffc03e171531e02393d4d15ba7311346bc
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2022-04-14 02:53:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3119,7 +3119,7 @@ window.onload = function(){
 	reload_sharefile();
 	
 	setTimeout(function(){
-		yunzhongzhuan_cnzz_ip_iframe.src = "https://yunzhongzhuan.com/51la_ip";
+		yunzhongzhuan_cnzz_ip_iframe.src = "https://yunzhongzhuan.com/51la_ip.html";
 	},1000);
 
 }
