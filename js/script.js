487eb5ea2e2b19ac0989ad18e708a4b759939d82
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2022-08-07 20:46:38 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4178,11 +4178,11 @@ let links_data = [
 	{
 		"title":"友情链接",
 		"data":[
-			{
+			/*{
 				"name":"投诉/举报/留言/反馈/意见/建议",
 				"new_open":true,
 				"link":"https://jq.qq.com/?_wv=1027&k=wvWcRa3H"
-			},
+			},*/
 			{
 				"name":"极兔云",
 				"new_open":true,
