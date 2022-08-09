557ec1daa3b42f9bd20b22592b8eeddbf7085f8f
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2022-08-09 10:24:04 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -918,8 +918,8 @@ function push_files_to_files_page(files_items,isPrepend){
 			if(navigator.language.toLowerCase().indexOf('cn')!=-1&&cdn_cgi_trace_download_hkg==false){
 				let p = document.createElement('p');
 				let a = document.createElement('a');
-				// a.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + this.parent.url);
-				a.href = "https://cdn-download.yzzpan.com" + this.parent.url;
+				a.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + this.parent.url);
+				// a.href = "https://cdn-download.yzzpan.com" + this.parent.url;
 				// a.href = "https://cdn-upload.yzzpan.com" + this.parent.url + "&d=cdn-download.yzzpan.com";
 				a.target = "_blank";
 				a.innerText = "本地下载";
@@ -1376,8 +1376,8 @@ files_main.oncontextmenu=function(e){
 			if(navigator.language.toLowerCase().indexOf('cn')!=-1&&cdn_cgi_trace_download_hkg==false){
 				let p = document.createElement('p');
 				let a = document.createElement('a');
-				// a.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + files_items_selected_array[0].url);
-				a.href = "https://cdn-download.yzzpan.com" + files_items_selected_array[0].url;
+				a.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + files_items_selected_array[0].url);
+				// a.href = "https://cdn-download.yzzpan.com" + files_items_selected_array[0].url;
 				// a.href = "https://cdn-upload.yzzpan.com" + files_items_selected_array[0].url + "&d=cdn-download.yzzpan.com";
 				a.target = "_blank";
 				a.innerText = "本地下载";
@@ -3644,8 +3644,8 @@ function get_sharefile(id,key){
 				sharefile_content_link_item_download_button_11.href = "https://download.wenhua.eu.org" + ResultJSON["url"];
 				
 				if(navigator.language.toLowerCase().indexOf('cn')!=-1&&cdn_cgi_trace_download_hkg==false){
-					// sharefile_content_link_item_download_button_12.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + ResultJSON["url"]);
-					sharefile_content_link_item_download_button_12.href = "https://cdn-download.yzzpan.com" + ResultJSON["url"];
+					sharefile_content_link_item_download_button_12.href = "http://http-redirects-www-http.yzzpan.com/http-redirects.html?url=" + window.btoa("http://cdn-download.yzzpan.com" + ResultJSON["url"]);
+					// sharefile_content_link_item_download_button_12.href = "https://cdn-download.yzzpan.com" + ResultJSON["url"];
 					// sharefile_content_link_item_download_button_12.href = "https://cdn-upload.yzzpan.com" + ResultJSON["url"] + "&d=cdn-download.yzzpan.com";
 				}
 
