a33fde2f1990753a4163c82f41c3a70d8bc10c3e
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2022-08-12 16:13:07 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -897,7 +897,7 @@ function push_files_to_files_page(files_items,isPrepend){
 		div.menu_download_element.parent = div;
 		div.menu_download_element.onclick = function(){
 			let html_element = document.createElement('div');
-			html_element.innerHTML = "<p><a href='https://download.yzzpan.com" + this.parent.url + "' target='_blank'>电信下载</a></p><p><a href='https://cdn-download.yunzhongzhuan.xyz" + this.parent.url + "' target='_blank'>移动下载</a></p><p><a href='https://ddos-guard-net-download.yunzhongzhuan.com" + this.parent.url + "' target='_blank'>联通下载</a></p><p><a href='https://download.yunzhongzhuan.eu.org" + this.parent.url + "' target='_blank'>备用下载</a></p><p><a href='https://download.yantudefengjing.eu.org" + this.parent.url + "' target='_blank'>备用下载</a></p><p><a href='https://download.wenhua.eu.org" + this.parent.url + "' target='_blank'>备用下载</a></p>";
+			html_element.innerHTML = "<p><a href='https://download.yzzpan.com" + this.parent.url + "' target='_blank'>电信下载（稳定）</a></p><p><a href='https://cdn-download.yunzhongzhuan.xyz" + this.parent.url + "' target='_blank'>移动下载（稳定）</a></p><p><a href='https://ddos-guard-net-download.yunzhongzhuan.com" + this.parent.url + "' target='_blank'>联通下载（稳定）</a></p><p><a href='https://download.yunzhongzhuan.eu.org" + this.parent.url + "' target='_blank'>备用下载（稳定）</a></p><p><a href='https://download.yantudefengjing.eu.org" + this.parent.url + "' target='_blank'>备用下载（稳定）</a></p><p><a href='https://download.wenhua.eu.org" + this.parent.url + "' target='_blank'>备用下载（稳定）</a></p>";
 			if(this.parent.offline!=undefined){
 				let p = document.createElement('p');
 				let a = document.createElement('a');
@@ -979,7 +979,7 @@ function push_files_to_files_page(files_items,isPrepend){
 				// a.href = "https://cdn-download.yzzpan.com" + this.parent.url;
 				// a.href = "https://cdn-upload.yzzpan.com" + this.parent.url + "&d=cdn-download.yzzpan.com";
 				a.target = "_blank";
-				a.innerText = "玄学下载";
+				a.innerText = "玄学下载（不稳定）";
 				p.append(a);
 				html_element.append(p);
 			}
@@ -1411,7 +1411,7 @@ files_main.oncontextmenu=function(e){
 		}
 		files_download_button.onclick = function(){
 			let html_element = document.createElement('div');
-			html_element.innerHTML = "<p><a href='" + download_web_url + files_items_selected_array[0].url + "' target='_blank'>电信下载</a></p><p><a href='https://cdn-download.yunzhongzhuan.xyz" + files_items_selected_array[0].url + "' target='_blank'>移动下载</a></p><p><a href='https://ddos-guard-net-download.yunzhongzhuan.com" + files_items_selected_array[0].url + "' target='_blank'>联通下载</a></p><p><a href='https://download.yunzhongzhuan.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载</a></p><p><a href='https://download.yantudefengjing.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载</a></p><p><a href='https://download.wenhua.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载</a></p>";
+			html_element.innerHTML = "<p><a href='" + download_web_url + files_items_selected_array[0].url + "' target='_blank'>电信下载（稳定）</a></p><p><a href='https://cdn-download.yunzhongzhuan.xyz" + files_items_selected_array[0].url + "' target='_blank'>移动下载（稳定）</a></p><p><a href='https://ddos-guard-net-download.yunzhongzhuan.com" + files_items_selected_array[0].url + "' target='_blank'>联通下载（稳定）</a></p><p><a href='https://download.yunzhongzhuan.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载（稳定）</a></p><p><a href='https://download.yantudefengjing.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载（稳定）</a></p><p><a href='https://download.wenhua.eu.org" + files_items_selected_array[0].url + "' target='_blank'>备用下载（稳定）</a></p>";
 			if(files_items_selected_array[0].offline!=undefined){
 				let p = document.createElement('p');
 				let a = document.createElement('a');
@@ -1494,7 +1494,7 @@ files_main.oncontextmenu=function(e){
 				// a.href = "https://cdn-download.yzzpan.com" + files_items_selected_array[0].url;
 				// a.href = "https://cdn-upload.yzzpan.com" + files_items_selected_array[0].url + "&d=cdn-download.yzzpan.com";
 				a.target = "_blank";
-				a.innerText = "玄学下载";
+				a.innerText = "玄学下载（不稳定）";
 				p.append(a);
 				html_element.append(p);
 			}
