4113ff1d3c87ff1fcda65b1058f84f1a1c9c4be8
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2022-09-09 14:19:00 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2660,7 +2660,7 @@ function cdn_cgi_trace_download(){
 		if(xmlhttp.readyState==4 && xmlhttp.status==200){
 			// console.log(xmlhttp.responseText);
 			cdn_cgi_trace_download_locked = true;
-			if(xmlhttp.responseText.indexOf('colo=HKG--')!=-1||xmlhttp.responseText.indexOf('colo=FRA--')!=-1||xmlhttp.responseText.indexOf('colo=ICN--')!=-1){
+			if(xmlhttp.responseText.indexOf('colo=HKG')!=-1||xmlhttp.responseText.indexOf('colo=FRA--')!=-1||xmlhttp.responseText.indexOf('colo=ICN--')!=-1){
 				cdn_cgi_trace_download_hkg = true;
 				sharefile_content_link_items_cdn_download.style.display = "none";
 			}
