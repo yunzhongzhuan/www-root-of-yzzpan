af3455e89773958d6016ae23286fe39425b808a6
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2023-03-12 18:02:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -375,7 +375,7 @@ let top_apps = [
 	{
 		"name":"云中转",
 		"link":"https://www.yzzpan.com/#files",
-		"description":"云中转-免费云盘不限容量不限速度不限流量全球网络传输文件",
+		"description":"云中转-全球网络传文件不限速",
 		"icon":"/img/logo.png",
 		// "icon":"//tva1.sinaimg.cn/mw2000/005CbWF8gy1h16lkvt5ubj30go0goq4h.jpg",
 		"new_open":false
