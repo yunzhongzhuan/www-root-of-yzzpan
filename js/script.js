2001efc7310b27060cb4e7f04b778b4ca8c3fe9b
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-04-18 15:14:13 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5920,6 +5920,16 @@ function setSessid(){
 setSessid();
 // 上传
 function FileUploadStart(fb,so){
+
+	// 通过非法修改前端，强制提交数据，后端判断为异常，将直接永久封号。
+	// 如果容量超额，禁止继续上传。
+ 	if( usedsize >= 2*100*1024*1024*1024 && public_link_status==false ){
+  		alert('您的账号存储空间占用已超200GB，已达个人普通账号上限2倍。\r\n企业用户不限容量，并支持绑定自定义下载域名。\r\n详情：https://www.yunzhongzhuan.com/welcome/');
+      		free_or_mini_or_big_vip.className = "";
+  		window.location.href="/#setting";
+  		return false;
+	}
+
 	// 如果未登录
 	if(userinfo["id"]!=undefined&&userinfo["id"]!=undefined!=null&&userinfo["id"]!=undefined!=''&&userinfo["qq"]!=undefined&&userinfo["qq"]!=undefined!=null&&userinfo["qq"]!=undefined!=''){
 		// pass
