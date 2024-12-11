57bfec7e6c6328a0ca04f56e4afc5731ab7054f5
yunzhongzhuan <103282114+yunzhongzhuan@users.noreply.github.com>
2024-12-11 16:45:19 +0800

    Update login.js

--- KOS/js/login.js
+++ KOS/js/login.js
@@ -5,14 +5,14 @@
 
 
 
-// login.html
+// login
 if(document.getElementsByClassName('login-main')[0]!=undefined){
 
 
 
 	document.getElementsByClassName('login-main')[0].getElementsByClassName('login')[0].onclick = function(){
 
-		window.location.href = "buckets.html"; // buckets.html
+		window.location.href = "buckets"; // buckets
 
 	}
 
